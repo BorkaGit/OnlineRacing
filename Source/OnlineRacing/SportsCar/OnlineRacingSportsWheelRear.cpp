b4385f08109649616e90c7463a81cb11badcc0df
Volodymyr Babkin <vvovkin1@gmail.com>
2026-08-17 20:00:15 +0200

    * SportsCar wheels' parameters adjusted

--- Source/OnlineRacing/SportsCar/OnlineRacingSportsWheelRear.cpp
+++ Source/OnlineRacing/SportsCar/OnlineRacingSportsWheelRear.cpp
@@ -8,8 +8,9 @@ UOnlineRacingSportsWheelRear::UOnlineRacingSportsWheelRear()
 	WheelRadius = 40.f;
 	WheelWidth = 40.0f;
 	FrictionForceMultiplier = 4.0f;
+	SideSlipModifier = 0.6f;
 	SlipThreshold = 100.0f;
 	SkidThreshold = 100.0f;
 	MaxSteerAngle = 0.0f;
-	MaxHandBrakeTorque = 6000.0f;
+	MaxHandBrakeTorque = 8000.0f;
 }
